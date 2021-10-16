8c7de28d72206ae946ad6ae811dc2c36c53707e3
Jaewhan <ghksdl1595@naver.com>
2021-10-16 14:03:44 +0100

    Added LED for visualizing of sampling

--- datacollection/datacollection.ino
+++ datacollection/datacollection.ino
@@ -1,22 +1,34 @@
+const int RED = 22;
+const int GREEN = 23;
+int n = 0;
+
 void setup() {
   Serial.begin(9600);
-  int n = 0;
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, HIGH);
+  Serial.println("Be Ready in 5 seconds");
+  digitalWrite(RED, LOW);
+  delay(5000);
+  digitalWrite(RED, HIGH);
 }
 
 void loop() {
-  float mav = 0;
+  digitalWrite(GREEN, LOW);
   float emg = 0;
+  float mav = 0;
   for (int j = 0; j < 1000; j++) {
     for (int i = 0; i < 100; i++) {
     emg = analogRead(A0) / 1024;
-    mav += emg;
     }
   mav = mav / 100;
   Serial.print(mav);
   delay(1);
   }
   n += 1;
-  Serial.println("%d[s]",n);
+  Serial.println(n);
+  digitalWrite(GREEN, HIGH);
   if (n == 100) {
    Serial.println('%'); 
   }
